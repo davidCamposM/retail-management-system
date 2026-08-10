69276c0ea4a885a7bef0579ffabe8d381a1bb4e4
davidCamposM <udecamtres@gmail.com>
2026-08-09 20:35:05 -0400

    Elimina scaffold Node sin usar y config de herramientas de IA del repo

--- backend/node/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { server } from "./api/server.js";
-import { port } from "./config.js";
-import packageJson from "../package.json" with { type: "json" };
-
-console.log(`Starting ${packageJson.name} v${packageJson.version}...`);
-
-server.listen(port, () => {
-  console.log("Server listening", server.address());
-});
